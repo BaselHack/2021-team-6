46e8811254f9adc9543f9f7ee51e77afc13d3d60
yveswehrli <yves.wehrli@bluewin.ch>
2021-09-11 12:09:19 +0200

    Connect firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD7cyRtkVR0Rx4HpZTEcCrjTM_HFysjPG0",
+    authDomain: "guess-what-33aa2.firebaseapp.com",
+    projectId: "guess-what-33aa2",
+    storageBucket: "guess-what-33aa2.appspot.com",
+    messagingSenderId: "433516479852",
+    appId: "1:433516479852:web:65820000a56e0c6f12df49"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD7cyRtkVR0Rx4HpZTEcCrjTM_HFysjPG0",
+    authDomain: "guess-what-33aa2.firebaseapp.com",
+    projectId: "guess-what-33aa2",
+    storageBucket: "guess-what-33aa2.appspot.com",
+    messagingSenderId: "433516479852",
+    appId: "1:433516479852:web:65820000a56e0c6f12df49"
+  }
 };
 
 /*
