d9b5b6671158d182eef8a1c37223fedd4913c0f9
InnigerM <marco.inniger@gmail.com>
2021-09-11 15:43:32 +0200

    Added lobbycode to lobbyService

--- src/app/services/lobby.service.ts
+++ src/app/services/lobby.service.ts
@@ -1,29 +1,40 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument, } from '@angular/fire/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Lobby, User } from '../models/lobby.model';
 import firebase from 'firebase/app';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LobbyService {
+  public lobbyCode: string;
 
-  constructor(public afs: AngularFirestore) { }
+  constructor(public afs: AngularFirestore) {}
 
   public createLobby(lobbyCode: string, lobby: Lobby) {
+    this.lobbyCode = lobbyCode;
     return this.afs.collection('lobbies').doc(lobbyCode).set(lobby);
   }
 
   public getLobby(lobbyCode: string): Observable<Lobby> {
-    const lobbyRef: AngularFirestoreDocument<Lobby> = this.afs.doc(`lobbies/` + lobbyCode);
+    const lobbyRef: AngularFirestoreDocument<Lobby> = this.afs.doc(
+      `lobbies/` + lobbyCode
+    );
     return lobbyRef.valueChanges();
   }
 
   public joinLobby(lobbyCode: string, user: User) {
-    return this.afs.collection('lobbies').doc(lobbyCode).update({
-      users: firebase.firestore.FieldValue.arrayUnion(user)
-    });
+    this.lobbyCode = lobbyCode;
+    return this.afs
+      .collection('lobbies')
+      .doc(lobbyCode)
+      .update({
+        users: firebase.firestore.FieldValue.arrayUnion(user),
+      });
   }
-
 }
