98e043188c84f052cb0aa1b042452b7c874f1220
InnigerM <marco.inniger@gmail.com>
2021-09-11 13:31:40 +0200

    Added lobby screen flow

--- src/app/lobby/view-lobby/view-lobby.page.ts
+++ src/app/lobby/view-lobby/view-lobby.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LobbyService } from 'src/app/services/lobby.service';
 
 @Component({
   selector: 'app-view-lobby',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-lobby.page.scss'],
 })
 export class ViewLobbyPage implements OnInit {
+  public lobbyCode = '1234';
+  public isHost = true;
 
-  constructor() { }
+  constructor(private lobbySvc: LobbyService, private router: Router) {}
 
   ngOnInit() {
+    // this.lobbySvc.lobbyCode.subscribe((code) => {
+    //   this.lobbyCode = code;
+    // });
   }
 
+  public onStartGame(): void {
+    console.log('Game started...');
+    this.router.navigateByUrl('/answer-question');
+  }
 }
