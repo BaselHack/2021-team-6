86dfe821f0fa0db641542cfc7b885ab1dc6a183e
InnigerM <marco.inniger@gmail.com>
2021-09-11 13:08:40 +0200

    Fixed routing error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,39 +4,62 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
+  },
+  {
+    path: 'create-lobby',
+    loadChildren: () =>
+      import('./lobby/create-lobby/create-lobby.module').then(
+        (m) => m.CreateLobbyPageModule
+      ),
   },
   {
     path: 'join-lobby',
-    loadChildren: () => import('./lobby/join-lobby/join-lobby.module').then( m => m.JoinLobbyPageModule)
+    loadChildren: () =>
+      import('./lobby/join-lobby/join-lobby.module').then(
+        (m) => m.JoinLobbyPageModule
+      ),
   },
   {
     path: 'view-lobby',
-    loadChildren: () => import('./lobby/view-lobby/view-lobby.module').then( m => m.ViewLobbyPageModule)
+    loadChildren: () =>
+      import('./lobby/view-lobby/view-lobby.module').then(
+        (m) => m.ViewLobbyPageModule
+      ),
   },
   {
     path: 'answer-question',
-    loadChildren: () => import('./game/answer-question/answer-question.module').then( m => m.AnswerQuestionPageModule)
+    loadChildren: () =>
+      import('./game/answer-question/answer-question.module').then(
+        (m) => m.AnswerQuestionPageModule
+      ),
   },
   {
     path: 'rate-answers',
-    loadChildren: () => import('./game/rate-answers/rate-answers.module').then( m => m.RateAnswersPageModule)
+    loadChildren: () =>
+      import('./game/rate-answers/rate-answers.module').then(
+        (m) => m.RateAnswersPageModule
+      ),
   },
   {
     path: 'scoreboard',
-    loadChildren: () => import('./game/scoreboard/scoreboard.module').then( m => m.ScoreboardPageModule)
+    loadChildren: () =>
+      import('./game/scoreboard/scoreboard.module').then(
+        (m) => m.ScoreboardPageModule
+      ),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
