006ccb56b43ee063021f31cd15a0af7bac473bcb
InnigerM <marco.inniger@gmail.com>
2021-09-12 13:02:50 +0200

    Fixed dimensions of barchart

--- src/app/game/scoreboard/scoreboard.page.ts
+++ src/app/game/scoreboard/scoreboard.page.ts
@@ -17,16 +17,11 @@ export class ScoreboardPage implements OnInit, OnDestroy {
   public lobbyCode;
   public questions: Question[];
   public currentQuestion: Question;
-  private lobbySubscription: Subscription;
-  private dimensions;
-
   public guessChart: GuessChart[] = [];
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-    private lobbyService: LobbyService
-  ) {}
+  private lobbySubscription: Subscription;
+
+  constructor(private lobbyService: LobbyService) {}
 
   ngOnInit() {
     this.lobbyCode = this.lobbyService.getLobbyCode();
@@ -34,32 +29,45 @@ export class ScoreboardPage implements OnInit, OnDestroy {
       this.lobby = lobby;
       this.questions = lobby.questions;
 
-      let formattedGuesses = new Map<string, Map<string, number>>();
+      const formattedGuesses = new Map<string, Map<string, number>>();
 
-      this.lobby.userGuesses.forEach(userGuess => {
+      this.lobby.userGuesses.forEach((userGuess) => {
         console.log(userGuess);
-        userGuess.guesses.forEach(guess => {
-          let guessChart: GuessChart = this.guessChart.find(x => x.answer === guess.answer);
+        userGuess.guesses.forEach((guess) => {
+          const guessChart: GuessChart = this.guessChart.find(
+            (x) => x.answer === guess.answer
+          );
           if (!guessChart) {
-            let newGuessChart: GuessChart = {answer: guess.answer, data: [{name: this.getUsername(guess.userId), value: 1}]}
+            const newGuessChart: GuessChart = {
+              answer: guess.answer,
+              data: [{ name: this.getUsername(guess.userId), value: 1 }],
+            };
             this.guessChart.push(newGuessChart);
           } else {
-            let guessData = guessChart.data.find(x => x.name === this.getUsername(guess.userId));
-            let index = guessChart.data.findIndex(x => x.name === this.getUsername(guess.userId));
-            if(!guessData) {
-              guessChart.data.push({name: this.getUsername(guess.userId), value: 1});
+            const guessData = guessChart.data.find(
+              (x) => x.name === this.getUsername(guess.userId)
+            );
+            const index = guessChart.data.findIndex(
+              (x) => x.name === this.getUsername(guess.userId)
+            );
+            if (!guessData) {
+              guessChart.data.push({
+                name: this.getUsername(guess.userId),
+                value: 1,
+              });
             } else {
               guessData.value++;
               guessChart.data[index] = guessData;
               console.log('else');
             }
+            guessChart.dimensions = this.getDimensions(guessChart);
           }
         });
       });
       console.log(this.guessChart);
       formattedGuesses.forEach((userGuess, question) => {
         console.log(question, userGuess);
-      })
+      });
     });
   }
 
@@ -67,8 +75,9 @@ export class ScoreboardPage implements OnInit, OnDestroy {
     return this.lobby.users?.find((x) => x.id === userID)?.username;
   }
 
-  getDimensions(guess: GuessChart) {
-    return [guess.data.length * 50, window.innerWidth];
+  getDimensions(guess: GuessChart): [number, number] {
+    const chartMargins = 26;
+    return [window.innerWidth - 50, guess.data.length * 50 + chartMargins];
   }
 
   ngOnDestroy() {
--- src/app/models/Guess.model.ts
+++ src/app/models/Guess.model.ts
@@ -1,14 +1,11 @@
 export interface UserGuess {
   userID: string;
-  guesses:
-   { answer: string,
-    userId: string}[]
-
+  guesses: { answer: string; userId: string }[];
 }
-
 
 export interface GuessChart {
   answer: string;
+  dimensions?: [number, number];
   data: {
     name: string;
     value: number;
